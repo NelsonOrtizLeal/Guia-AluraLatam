2f6b395650e0c73b027d569311ccbf88d6befa86
Nelson Ortiz Leal <nelsonortizleal@gmail.com>
2024-02-13 21:14:57 -0600

    Update: Agregando uso de comentarios y template Strings

--- 1-Logica de programacion/app.js
+++ 1-Logica de programacion/app.js
@@ -1,8 +1,17 @@
+// Variables
 let numeroSecreto = 6;
 let numeroUsuario = prompt("Me indicas un número por favor:");
 
 console.log(numeroUsuario);
 
+/*!SECTION
+Este código realiza
+la comparación
+*/
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
+    //Acertamos, fue verdadero la condición
+    alert(`Acertaste, el número es: ${numeroUsuario}`);
+}else{
+    //La condición no se cumplio
+    alert('Lo siento, no acertaste el numero.')
 }
