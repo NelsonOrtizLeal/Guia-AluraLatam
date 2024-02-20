db7ce0a2ba96ca5fab8ec85e5e848cfcbd349637
Nelson Ortiz Leal <nelsonortizleal@gmail.com>
2024-02-19 22:35:21 -0600

    Update:Uso de función Math.Random()

--- 1-Logica de programacion/app.js
+++ 1-Logica de programacion/app.js
@@ -1,24 +1,24 @@
 // Variables
-let numeroSecreto = 6;
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = "vez";
+// let palabraVeces = "vez";
 let maximoIntentos = 3;
 
-console.log(numeroUsuario);
+// console.log(numeroSecreto);
 
 /*!SECTION
 Este código realiza
 la comparación
 */
 while (numeroUsuario != numeroSecreto) {
-  numeroUsuario = prompt("Me indicas un número por favor:");
+  numeroUsuario = parseInt(prompt("Me indicas un número por favor:"));
 
-  if (numeroUsuario == numeroSecreto) {
+  if (numeroUsuario === numeroSecreto) {
     //Acertamos, fue verdadero la condición
     alert(
       `Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${
-        intentos == 1 ? palabraVeces : "veces"
+        intentos == 1 ? "vez" : "veces"
       }.`
     );
   } else {
