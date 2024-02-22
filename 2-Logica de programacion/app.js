fc67b56e7dccd8a7c2bc1ac364decee9eb6e2aaa
Nelson Ortiz Leal <nelsonortizleal@gmail.com>
2024-02-21 22:31:36 -0600

    Update:Agregando mensaje si acertó, numero de intentos, limpiar campo y función para reiniciar juego

--- 2-Logica de programacion/app.js
+++ 2-Logica de programacion/app.js
@@ -1,12 +1,10 @@
-let numeroAleatorio = generarNumeroAleatorio();
+let numeroAleatorio = 0;
+let intentos = 0;
 
 function asignarTextoElemento(elemento, text) {
   let elementoHTML = document.querySelector(elemento);
   elementoHTML.innerHTML = text;
 }
-
-asignarTextoElemento("h1", "Juego del número secreto!");
-asignarTextoElemento("p", "Indica un número del 1 al 10");
 
 function generarNumeroAleatorio() {
   let numeroAleatorio = Math.floor(Math.random() * 10) + 1;
@@ -16,9 +14,47 @@ function generarNumeroAleatorio() {
 
 function verificarNumero() {
   let numeroUsuario = parseInt(document.getElementById("numeroUsuario").value);
-  console.log(typeof numeroUsuario);
-  console.log(typeof(numeroAleatorio));
-  console.log(numeroUsuario === numeroAleatorio);
-  //alert("Has hecho click");
+
+  if (numeroUsuario === numeroAleatorio) {
+    asignarTextoElemento(
+      "p",
+      `Felicidades acertaste el numero secreto en ${intentos} ${
+        intentos === 1 ? "vez" : "veces"
+      }`
+    );
+    document.getElementById("reiniciar").removeAttribute("disabled");
+  } else {
+    if (numeroUsuario > numeroAleatorio) {
+      asignarTextoElemento("p", "El numero secreto es menor");
+    } else {
+      asignarTextoElemento("p", "El numero secreto es mayor");
+    }
+    intentos++;
+    limpiarCaja();
+  }
+  return;
 }
 
+function limpiarCaja() {
+  document.querySelector("#numeroUsuario").value = "";
+}
+
+function iniciarValores() {
+  asignarTextoElemento("h1", "Juego del número secreto!");
+  asignarTextoElemento("p", "Indica un número del 1 al 10");
+  numeroAleatorio = generarNumeroAleatorio();
+  intentos = 1;
+}
+
+function reiniciarJuego() {
+  //limpiar la caja
+  limpiarCaja();
+  //Colocar los mensajes de nuevo
+  //Calculaar numero aleatorio
+  //reiniciar los intentos
+  iniciarValores();
+  //Deshabilitar el boton
+  document.getElementById("reiniciar").setAttribute("disabled", true);
+}
+
+iniciarValores();
